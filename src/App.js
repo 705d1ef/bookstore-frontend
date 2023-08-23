f351c09635ddbb9ee22397d43960f8d429e98ee2
Fiodor <307605post@gmail.com>
2023-08-23 03:28:49 +0100

    add cart-block

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/home-page";
+import { Header } from "./components/header";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Routes>
           <Route path="/" element={ <HomePage /> } />
         </Routes>
