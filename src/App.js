23341e394f48c46ad049b40953179e9ca84ab590
Fiodor <307605post@gmail.com>
2023-08-25 01:25:57 +0100

    add to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/home-page";
 import { Header } from "./components/header";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
         <Header />
         <Routes>
           <Route path="/" element={ <HomePage /> } />
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
