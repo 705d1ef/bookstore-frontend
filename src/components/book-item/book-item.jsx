a6eab1673f7a3e0d9dc4879b4e6faba55a55bd21
Fiodor <307605post@gmail.com>
2023-08-27 02:39:28 +0100

    add component book-item

---
+++ src/components/book-item/book-item.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BookCover } from "../book-cover";
+import "./book-item.css";
+
+export const BookItem = ({ book }) => {
+  return (
+    <div className="book-item">
+        <BookCover />
+      <div className="book-item__details"></div>
+      <span className="book-item__title">{book.title}</span>
+      <div className="book-item__author">{book.author}</div>
+      <div className="book-item__buy">Buy</div>
+    </div>
+  );
+};
