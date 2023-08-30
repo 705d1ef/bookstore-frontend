41bc72dd775b7cd378af76a000c7fd90ddbe9f01
Fiodor <307605post@gmail.com>
2023-08-30 03:44:41 +0100

    hujnia

--- src/components/book-buy/book-buy.jsx
+++ src/components/book-buy/book-buy.jsx
@@ -1,31 +1,14 @@
 import React from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
 import { Button } from "../button";
->>>>>>> create_button_GameBuy
-=======
-import { Button } from "../button";
->>>>>>> create_button_GameBuy
 import "./book-buy.css";
 
 export const BookBuy = ({ book }) => {
   return (
     <div className="book-buy">
       <span className="book-buy__price">{book.price}</span>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
       <Button type="primary" onClick={() => null}>
         To Basket
       </Button>
->>>>>>> create_button_GameBuy
-=======
-      <Button type="primary" onClick={() => null}>
-        To Basket
-      </Button>
->>>>>>> create_button_GameBuy
     </div>
   );
 };
