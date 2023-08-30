5ba41aea97041b6e93eb03ee479138f375493429
Fiodor <307605post@gmail.com>
2023-08-30 02:47:26 +0100

    code refactoring book-buy

--- src/components/book-buy/book-buy.jsx
+++ src/components/book-buy/book-buy.jsx
@@ -6,10 +6,8 @@ export const BookBuy = ({ book }) => {
   return (
     <div className="book-buy">
       <span className="book-buy__price">{book.price}</span>
-      <Button
-      type="primary"
-      onClick={() => null}>
-          To Basket
+      <Button type="primary" onClick={() => null}>
+        To Basket
       </Button>
     </div>
   );
