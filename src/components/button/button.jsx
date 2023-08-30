4272eecc9ab337352d71e30e181d550199b9455e
Fiodor <307605post@gmail.com>
2023-08-30 05:15:56 +0100

    add

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -4,7 +4,7 @@ import "./button.css";
 
 export const Button = ({ onClick, type, children, size = "s" }) => {
   const btnClass = classNames({
-    "btn": true,
+    btn: true,
     "btn-secondary": type === "secondary",
     "btn-primary": type === "primary",
     "btn--small": size === "s",
