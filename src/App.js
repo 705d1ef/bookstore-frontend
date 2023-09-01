438d498516cfa91ba50cba6b98d41f1b88d92f82
Fiodor <307605post@gmail.com>
2023-09-01 03:30:51 +0100

    add store and provider to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/home-page";
 import { Header } from "./components/header";
+import { Provider } from "react";
+import { store } from "./store";
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className="App">
-                <Header />
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                </Routes>
-            </div>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <div className="App">
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
