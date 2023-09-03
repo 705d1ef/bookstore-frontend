7c61b10a83fd1b508f49b35136c99607c8ad53cb
Fiodor <307605post@gmail.com>
2023-09-03 03:28:10 +0100

    add handleClick function

--- src/components/book-buy/book-buy.jsx
+++ src/components/book-buy/book-buy.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setItemInCart } from "../../store/cart/reducer";
 import { Button } from "../button";
 import "./book-buy.css";
 
 export const BookBuy = ({ book }) => {
-  return (
-    <div className="book-buy">
-      <span className="book-buy__price">{book.price} £.</span>
-      <Button type="primary" onClick={() => null}>
-        Add to Card
-      </Button>
-    </div>
-  );
+    const dispatch = useDispatch();
+    const handleClick = (e) => {
+        e.stopPropagation();
+        dispatch(setItemInCart(book));
+    };
+    return (
+        <div className="book-buy">
+            <span className="book-buy__price">{book.price} £.</span>
+            <Button type="primary" onClick={handleClick}>
+                Add to Card
+            </Button>
+        </div>
+    );
 };
